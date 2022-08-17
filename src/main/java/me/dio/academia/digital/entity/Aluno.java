743ff32c4a7ff4fe6790c5f27b91ffd631500898
heliowl <heliowl@hotmail.com>
2022-08-17 13:34:52 -0300

    comit inicial

---
+++ src/main/java/me/dio/academia/digital/entity/Aluno.java
@@ -0,0 +1,37 @@
+package me.dio.academia.digital.entity;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_alunos")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class Aluno {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String nome;
+
+  @Column(unique = true)
+  private String cpf;
+
+  private String bairro;
+
+  private LocalDate dataDeNascimento;
+
+  @OneToMany(mappedBy = "aluno", fetch = FetchType.LAZY)
+  @JsonIgnore
+  private List<AvaliacaoFisica> avaliacoes = new ArrayList<>();
+
+}
